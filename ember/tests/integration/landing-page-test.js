74bd621b02e606f0a638bb20373dddf370391b6b
Alex Beeken and David Zulaica <aeb242@gmail.com>
2015-04-13 16:24:28 -0700

    Install and Setup Ember

---
+++ ember/tests/integration/landing-page-test.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import { module, test } from 'qunit';
+import startApp from '../helpers/start-app';
+
+var App;
+
+module('Integration - Landing Page', {
+  beforeEach: function() {
+    App = startApp();
+  },
+  afterEach: function() {
+    Ember.run(App, 'destroy');
+  }
+});
+
+
+test('Should welcome me to BBWDTF', function(assert) {
+  visit('/').then(function() {
+    assert.equal(find('h1').text(), 'Welcome to BBWDTF');
+  });
+});
