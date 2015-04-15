c49afa954f052edef69bb0f0eec2499e96d77104
Guest User <student@epicodus.com>
2015-04-15 14:40:28 -0700

    Have Pusher on Rail (not set up fully)

--- ember/config/environment.js
+++ ember/config/environment.js
@@ -14,8 +14,11 @@ module.exports = function(environment) {
     },
 
     APP: {
-      // Here you can pass flags/options to your application instance
-      // when it is created
+      PUSHER_OPTS: {
+        key: '64c5a7610caea2d03ced',
+        connection: {},
+        logAllEvents: false
+      }
     }
   };
 
--- rails/config/environment.rb
+++ rails/config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Pusher.app_id = ENV['PUSHER_APP_ID']
+Pusher.key = ENV['PUSHER_KEY']
+Pusher.secret = ENV['PUSHER_SECRET']
