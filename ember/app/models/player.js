6e64a4f80a325e87f3b5f0fb6a68604a29445630
Guest User <student@epicodus.com>
2015-04-15 12:03:30 -0700

    Not Have Ember

--- ember/app/models/player.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  number: DS.attr('number'),
-  team: DS.belongsTo('team', { async: true })
-});
--- ember/app/models/team.js
+++
@@ -1,7 +0,0 @@
-import DS from 'ember-data';
-
-export default DS.Model.extend({
-  name: DS.attr('string'),
-  location: DS.attr('string'),
-  players: DS.hasMany('player', { async: true })
-});
