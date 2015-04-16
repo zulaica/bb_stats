6db14e28ff8d001d96fa2cd581fb3421575262e8
Alex Beeken and David Zulaica <zulaica@gmail.com>
2015-04-16 13:00:08 -0700

    Revise Seeds and Player layout on Team page.

--- rails/db/seeds.rb
+++ rails/db/seeds.rb
@@ -1,13 +1,11 @@
 teams = Team.create([
   { name: "Lightning Tigers",
     location: "Tillamook, OR" },
-  { name: "Lava Sharks",
-    location: "Albany, OR" },
   { name: "Space Panthers",
     location: "Eugene, OR" }
   ])
 
 Player.create(name: "Bob", number: "42", team: teams.first)
+Player.create(name: "Emily", number: "45", team: Team.first)
 Player.create(name: "Thaddeus", number: "421", team: teams.last)
 Player.create(name: "Jane", number: "48", team: teams.last)
-Player.create(name: "Emily", number: "45", team: Team.find(2))
